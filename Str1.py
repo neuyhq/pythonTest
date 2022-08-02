429f1d8912395a7c6425b028c49b1acc3372a440
neuyhq <neuyhq@sina.com>
2022-08-02 17:09:55 +0800

    Update Str1.py

--- Str1.py
+++ Str1.py
@@ -1,4 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 15:22:40 2022
+
+@author: neuyhq
+"""
+
 import os
+import sys
+import os
+import scapy
 
 print(os.path)
 print("test")
+print(os.path)
+print(len(sys.argv))
+a=scapy.VERSION
+print(a)
